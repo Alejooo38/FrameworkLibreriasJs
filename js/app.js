9d47a6fedbe2deec9db8ee8f7034d9bf02a0cfd7
DarbNho_78 <druizbermud@uniminuto.edu.co>
2019-06-12 10:44:42 -0500

    Hasta aqui funciona el punto 1. Titulo

--- js/app.js
+++ js/app.js
@@ -3,6 +3,45 @@ var CellSelected_y;
 var Movimientos;
 var puntuacion;
 
+//punto 1. cambia el color del titulo y alterna 
+function colorBlink(selector) {
+    $(selector).animate({
+            opacity: '1',
+        }, {
+            step: function () {
+                $(this).css('color', 'white');
+            },
+            queue: true
+        })
+        .animate({
+            opacity: '1'
+        }, {
+            step: function () {
+                $(this).css('color', 'yellow');
+            },
+            queue: true
+        }, 600)
+        .delay(1000)
+        .animate({
+            opacity: '1'
+        }, {
+            step: function () {
+                $(this).css('color', 'white');
+            },
+            queue: true
+        })
+        .animate({
+            opacity: '1'
+        }, {
+            step: function () {
+                $(this).css('color', 'yellow');
+                colorBlink('h1.main-titulo');
+            },
+            queue: true
+        });
+}
+
+
 var secs;
 var mins;
 var cronometer;
